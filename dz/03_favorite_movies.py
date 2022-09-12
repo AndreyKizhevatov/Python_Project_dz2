d9e4cbf62947a90e1d0da99b2451eab9992c91ea
Андрей <snezhkov.andrey@yandex.ru>
2022-09-12 16:54:02 +0300

    word

---
+++ dz/03_favorite_movies.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть строка с перечислением фильмов
+
+my_favorite_movies = 'Терминатор, Пятый элемент, Аватар, Чужие, Назад в будущее'
+
+# Выведите на консоль с помощью индексации строки, последовательно:
+#   первый фильм
+#   последний
+#   второй
+#   второй с конца
+
+# Переопределять my_favorite_movies и использовать .split() нельзя.
+# Запятая не должна выводиться.
+
+num1 = my_favorite_movies [0:10]
+last = my_favorite_movies [41:60]
+num2 = my_favorite_movies [11:25]
+num4 = my_favorite_movies [34:40]
+print(num1,last,num2,num4)
